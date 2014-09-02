33631367b8ddb7cdfe2b94deda56628aedb363f6
GrainRiver <zhaowenfeng@hotmail.com>
2014-09-02 13:03:50 +0800

    add a tips

--- test.c
+++ test.c
@@ -2,7 +2,7 @@
 int main(int argc, const char**argv)
 {
         printf("Hello world!\n");
-		printf("Em, this is branch 'dev' ...\n");
+		printf("Em, this is branch 'dev'. Please remember its purpose while working.\n");
 		printf("Bye!\n");
         return 0;
 }
